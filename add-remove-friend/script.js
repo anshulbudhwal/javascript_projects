1dfe311b1ebfa29e4afe8bc9ca1ce98de9007cfc
Anshul kumar <72787705+anshulbudhwal@users.noreply.github.com>
2023-11-02 15:54:02 +0530

    Add files via upload

---
+++ add-remove-friend/script.js
@@ -0,0 +1,21 @@
+var istatus = document.querySelector("h5")
+var btn = document.querySelector("#add")
+var check = 0;
+btn.addEventListener("click", function () {
+    if (check == 0) {
+        istatus.innerHTML = "Friends"
+        istatus.style.color = "green"
+        btn.innerHTML = "Remove Friend";
+        btn.style.backgroundColor = "#dadada"
+        btn.style.color = "black"
+        check = 1
+    }
+    else {
+        istatus.innerHTML = "Stranger"
+        istatus.style.color = "red"
+        btn.innerHTML = "Add Friend";
+        btn.style.backgroundColor = "cadetblue"
+        btn.style.color = "white"
+        check = 0
+    }
+})
